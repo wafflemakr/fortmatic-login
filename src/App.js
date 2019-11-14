411d03ba41f54a6b0bbd0a4c769b491e41c9f814
“wafflemakr” <wafflemakr@gmail.com>
2019-11-14 18:13:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+let fm = new window.Fortmatic(process.env.REACT_APP_FORTMATIC_API_KEY);
+const web3 = new window.Web3(fm.getProvider());
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async function execute() {
+      try {
+        await web3.currentProvider.enable();
+        let account = await web3.eth.getCoinbase();
+        let balance = await web3.eth.getBalance(account);
+        setUser({ account, balance });
+      } catch (e) {
+        setError("Error:" + e.message);
+      }
+    })();
+  }, [user]);
+
+  return user ? (
+    <>
+      <h1>Connected to: {user.account || null}</h1>
+      <h1>Balance: {web3.utils.fromWei(user.balance)} ETH</h1>
+    </>
+  ) : error ? (
+    <h1>{error}</h1>
+  ) : (
+    <h1>Connecting...</h1>
   );
 }
 
