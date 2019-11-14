de8dda1791f1b9933cd243cebcf9e69e84168741
“wafflemakr” <wafflemakr@gmail.com>
2019-11-14 18:58:50 -0500

    add metamask login, add some css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,74 @@
 import React, { useState, useEffect } from "react";
 
-let fm = new window.Fortmatic(process.env.REACT_APP_FORTMATIC_API_KEY);
-const web3 = new window.Web3(fm.getProvider());
+import "./App.css";
+
+let web3;
 
 function App() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const [loginFortmatic, setLoginFortmatic] = useState(false);
+  const [loginMetamask, setLoginMetamask] = useState(false);
 
   useEffect(() => {
-    (async function execute() {
-      try {
-        await web3.currentProvider.enable();
-        let account = await web3.eth.getCoinbase();
-        let balance = await web3.eth.getBalance(account);
-        setUser({ account, balance });
-      } catch (e) {
-        setError("Error:" + e.message);
-      }
-    })();
-  }, [user]);
+    if (loginFortmatic) {
+      (async function() {
+        try {
+          let fm = new window.Fortmatic(
+            process.env.REACT_APP_FORTMATIC_API_KEY
+          );
+          web3 = new window.Web3(fm.getProvider());
 
-  return user ? (
+          await web3.currentProvider.enable();
+          let account = await web3.eth.getCoinbase();
+          let balance = await web3.eth.getBalance(account);
+          setUser({ account, balance });
+        } catch (e) {
+          setError("Error:" + e.message);
+        }
+      })();
+    }
+  }, [loginFortmatic]);
+
+  useEffect(() => {
+    if (loginMetamask) {
+      (async function() {
+        try {
+          web3 = new window.Web3(window.ethereum);
+          await window.ethereum.enable();
+
+          let account = await web3.eth.getCoinbase();
+          let balance = await web3.eth.getBalance(account);
+          setUser({ account, balance });
+        } catch (e) {
+          setError("Error:" + e.message);
+        }
+      })();
+    }
+  }, [loginMetamask]);
+
+  const renderInfo = user ? (
     <>
-      <h1>Connected to: {user.account || null}</h1>
-      <h1>Balance: {web3.utils.fromWei(user.balance)} ETH</h1>
+      <p>Connected to: {user.account || null}</p>
+      <p>Balance: {web3.utils.fromWei(user.balance)} ETH</p>
     </>
-  ) : error ? (
-    <h1>{error}</h1>
-  ) : (
-    <h1>Connecting...</h1>
+  ) : null;
+
+  const renderError = error ? <h1>{error}</h1> : null;
+
+  return (
+    <div>
+      <div className="container">
+        {renderInfo}
+        {renderError}
+        <button className="login" onClick={() => setLoginFortmatic(true)}>
+          Connect with Fortmatic
+        </button>
+        <button className="login" onClick={() => setLoginMetamask(true)}>
+          Connect with Metamask
+        </button>
+      </div>
+    </div>
   );
 }
 
